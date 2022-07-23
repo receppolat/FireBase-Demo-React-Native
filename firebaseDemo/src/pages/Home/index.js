71b855f685234178c431b785c7599c3c21fb0551
Recep Polat <businessreceppolat@gmail.com>
2022-07-23 11:03:03 +0300

    initial Upload

---
+++ firebaseDemo/src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function index() {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
